d47a05cfcf18041fa60a6bb6979f62a5dcb84fae
K.Tsukimoto <t.29.wuq@gmail.com>
2018-11-11 15:28:37 +0900

    :sparkles: Corresponds to one character variable

--- cc.c
+++ cc.c
@@ -6,6 +6,7 @@
 enum{
     ND_NUM = 256,
     TK_NUM = 256, //integer token (token type num)
+    ND_IDENT,
     TK_EOF, //end of input
 };
 
@@ -20,10 +21,14 @@ typedef struct Node{
     struct Node *lhs;
     struct Node *rhs;
     int val;
+    char name;
 } Node;
 
 Token tokens[100];
 int pos = 0;
+int c_pos = 0;
+
+Node *code[100];
 
 Node *new_node(int op, Node *lhs, Node *rhs){
     Node *node = (Node*)malloc(sizeof(Node));
@@ -40,13 +45,44 @@ Node *new_node_num(int val){
      return node;
  }
 
+ Node *new_node_ident(char *name){
+     Node *node = (Node*)malloc(sizeof(Node));
+     node->ty = ND_IDENT;
+     node->name = *name;
+     return node;
+     
+ }
+
+void program();
+Node* assign();
 Node* expr();
 Node* mul();
 Node* term();
 
+void program(){
+    Node *lhs = assign();
+    code[c_pos++] = lhs;
+    if (tokens[pos].ty == TK_EOF)
+        return;
+    program();
+}
+
+Node* assign(){
+    Node* lhs = expr();
+    if (tokens[pos].ty == ';'){
+        pos++;
+        return lhs;
+    }
+    if (tokens[pos].ty == '='){
+        pos++;
+        return  new_node('=', lhs, assign());
+    }
+    return lhs;
+}
+
 Node* expr(){
     Node* lhs = mul();
-    if (tokens[pos].ty == TK_EOF || tokens[pos].ty == ')')
+    if (tokens[pos].ty == ')')
         return lhs;
     if (tokens[pos].ty == '+'){
         pos++;
@@ -56,7 +92,7 @@ Node* expr(){
         pos++;
         return new_node('-', lhs, expr());
     }
-    fprintf(stderr, "想定しないトークンです: %s", tokens[pos].input);
+    return lhs;
 }
 
 Node* mul(){
@@ -75,9 +111,10 @@ Node* mul(){
 }
 
 Node* term(){
-    if (tokens[pos].ty == TK_NUM)
-    
+    if (tokens[pos].ty == TK_NUM)  
         return new_node_num(tokens[pos++].val);
+    if (tokens[pos].ty == ND_IDENT)
+        return new_node_ident(tokens[pos++].input);
     if (tokens[pos].ty == '('){
         pos++;
         Node *node = expr();
@@ -87,10 +124,40 @@ Node* term(){
     fprintf(stderr, "数値でも開きカッコでもないトークンです: %s", tokens[pos].input);
 }
 
+void gen_lval(Node *node){
+    if (node->ty == ND_IDENT){
+        printf("\tmov rax, rbp\n");
+        printf("\tsub rax, %d\n", ('z' - node->name + 1) * 8);
+        printf("\tpush rax\n");
+        return;
+    }
+    fprintf(stderr, "代入の左辺値が変数ではありません");
+}
+
 void gen(Node *node){
     if (node->ty == ND_NUM){
         printf("\tpush %d\n", node->val);
         return;
+    }
+
+    if (node->ty == ND_IDENT){
+        gen_lval(node);
+        printf("\tpop rax\n");
+        printf("\tmov rax, [rax]\n");
+        printf("\tpush rax\n");
+        return;
+    }
+
+    if (node->ty == '='){
+        gen_lval(node->lhs);
+        gen(node->rhs);
+
+        printf("\tpop rdi\n");
+        printf("\tpop rax\n");
+        printf("\tmov [rax], rdi\n");
+        printf("\tpush rdi\n");
+        return;
+        
     }
 
     gen(node->lhs);
@@ -129,7 +196,8 @@ void tokenize(char *p){
 
         if (*p == '+' || *p == '-' ||
          *p == '*' || *p == '/' ||
-         *p == '(' || *p == ')'){
+         *p == '(' || *p == ')' ||
+         *p == '=' || *p == ';'){
             tokens[i].ty = *p;
             tokens[i].input = p;
             p++;
@@ -142,6 +210,14 @@ void tokenize(char *p){
             tokens[i].input = p;
             tokens[i].val = strtol(p, &p, 10);
             i++;
+            continue;
+        }
+
+        if ('a' <= *p && *p <= 'z'){
+            tokens[i].ty = ND_IDENT;
+            tokens[i].input = p;
+            i++;
+            p++;
             continue;
         }
 
@@ -166,15 +242,23 @@ int main(int argc, char **argv){
     }
     
     tokenize(argv[1]);
-    Node* node = expr();
+    program();
 
     printf(".intel_syntax noprefix\n");
     printf(".global _main\n");
     printf("_main:\n");
 
-    gen(node);
+    printf("\tpush rbp\n");
+    printf("\tmov rbp, rsp\n");
+    printf("\tsub rsp, 208\n");
 
-    printf("\tpop rax\n");
+    for (int i = 0;code[i];i++){
+        gen(code[i]);
+        printf("\tpop rax\n");
+    }
+
+    printf("\tmov rsp, rbp\n");
+    printf("\tpop rbp\n");
     printf("\tret\n");
     return 0;
     
